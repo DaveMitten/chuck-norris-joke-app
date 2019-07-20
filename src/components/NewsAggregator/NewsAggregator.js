d4a465d5f00a69814003bec12a463b4ae8f814f8
DaveMitten <davidmitten88@gmail.com>
2019-07-20 13:06:38 +0100

    installed react-app-rewired to allow for custom antd import to improve speed of app

---
+++ src/components/NewsAggregator/NewsAggregator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NewsAggregator extends Component {
+  render() {
+	return (
+	  <div>
+		hi
+	  </div>
+	)
+  }
+}
+
+
+export default NewsAggregator;
